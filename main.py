fd77561853a6570572c2a103b3b9a0b57e185183
Youngho-kr <Yyoung0997@gmail.com>
2024-04-15 17:12:00 +0900

    Update UX

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import threading
+import json
+
+import time
 
 app = Flask(__name__)
 CORS(app)
 
 messages = []
+message = []
 
 def make_answer(data):
-    messages.append({'user': 'OEY', 'message': f'My answer for ["{data['message']}]: GOOD!"'})
+    time.sleep(3)
+    message.append({"user": "OEY", "message": data['message']})
+    # print(type(data))
+
+def send_answer(message):
+    if len(message) == 0:
+        return json.loads('{"user": "NO_MESSAGE", "message": ""}'), 200
+    else:
+        print(message[0])
+        return message[0], 200
+        # return json.loads('{"user": "NO_MESSAGE", "message": ""}'), 200
+    # return json.loads('{"user": "NO_MESSAGE", "message": ""}')
 
 @app.route('/messages', methods=['GET', 'POST'])
 def handle_messages():
@@ -17,7 +33,9 @@ def handle_messages():
         make_answer(data)
         return jsonify({'status': 'success'}), 200
     elif request.method == 'GET':
-        return jsonify(messages), 200
+        _message = send_answer(message)
+        message.clear()
+        return send_answer(_message)
 
 if __name__ == '__main__':
     app.run(debug=True)
