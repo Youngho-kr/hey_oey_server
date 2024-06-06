b0691a54eb8f93f324af8aa9d9595c2debd929a5
darami819 <roadangel2002@gmail.com>
2024-06-06 14:49:34 +0900

    add: close model

--- app/models/model.py
+++ app/models/model.py
@@ -6,7 +6,19 @@ sys.path.insert(0, os.path.join(current_dir, "close_model"))
 print(os.path.join(current_dir, "close_model"))
 from .close_model.application import *
 
+# Close domain 모델검색 후 올바른 대답을 생성하지 못한 경우 Open domain 모델 응답 생성
 def process_message(message):
-    response, intent = generate_and_classify(message)
+    close_response = request_chat(message)
+    print(close_response)
+    close_state = close_response["state"]
+    close_message = close_response["answer"]
+    
+    if close_state == "SUCCESS":
+        return close_message, "경청"
 
-    return response, intent
+    elif close_state == "FALLBACK":
+        return generate_and_classify(message)
+
+    else:
+        return "자세히 말씀해주세요", "경청"
+
